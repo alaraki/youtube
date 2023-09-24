2799a51e9136db50d9bf991d3603701f40524496
alaraki <alaraki2000@hotmail.com>
2023-09-24 05:41:35 +0300

    Add files via upload

---
+++ data/cart.js
@@ -0,0 +1,55 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },
+  {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  },
+  {
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 3
+  }
+  ];
+}
+
+function saveToStorage(){
+localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId){
+  let matchingItemm;
+
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+  matchingItemm = cartItem;
+    }
+  });
+
+  if(matchingItemm){
+    matchingItemm.quantity += 1;
+  }else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+          });
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId){
+const newCart = [];
+cart.forEach((cartItem) => {
+if(cartItem.productId !== productId){
+  newCart.push(cartItem);
+}
+});
+
+cart = newCart;
+saveToStorage();
+}
